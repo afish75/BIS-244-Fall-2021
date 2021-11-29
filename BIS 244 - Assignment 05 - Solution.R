b593bc0cab51c0524c56c3c1f1da5e2ece2fe8f4
Troy Adair <tra418@lehigh.edu>
2021-11-29 14:02:43 -0500

    Assignment 05 Solution

--- BIS 244 - Assignment 05 - Solution.R
+++ BIS 244 - Assignment 05 - Solution.R
@@ -1,4 +1,4 @@
-# BIS 044 - Assignment 05 Solution
+# BIS 244 - Assignment 05 Solution
 
 library(tidyverse)
 
@@ -7,7 +7,7 @@ rm(list=ls(all=TRUE))
 cat("\014")
 
 tonnage <- read.csv("TBQ05.20211116133909.csv")
-str(tonnage)
+
 str(tonnage)
 tonnage$VALUE <- as.numeric(tonnage$VALUE)
 tonnage$VALUE[is.na(tonnage$VALUE)] <- 0
@@ -18,9 +18,25 @@ by(tonnage$VALUE,tonnage$Region.of.Trade,sum)
 tonnage<-tonnage[which(tonnage$Port!="All Main Irish Ports" &
                          tonnage$C01713V02044 =="1"),]
 
-ggplot(data=tonnage) + 
+p <- ggplot(data=tonnage) +
   geom_point(aes(x=Quarter,y=VALUE, color=Port)) + 
   ggtitle("Quarterly Tonnage Arriving From Great Britain and Northern Ireland") + 
   xlab("Quarter") + 
   ylab("Tonnage") +
   theme_minimal()
+
+p
+
+if (!require("ggthemes")) install.packages("ggthemes")
+library(ggthemes)
+
+ls(pattern = '^theme_', env = as.environment('package:ggthemes'))
+
+theme_set(theme_wsj())
+p + theme(legend.position="top")
+
+library(RColorBrewer)
+
+p + scale_color_brewer(palette = "Greens") +
+  theme(legend.position = "top")
+
